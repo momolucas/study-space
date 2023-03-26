4a06676c89aaff17a8da794d051554975bd41286
Lucas Momo <lucs.momo@gmail.com>
2023-03-25 21:40:40 -0300

    :wrench: config: organized project structure and kotlin dsl

---
+++ Android/StudySamples/core/build.gradle.kts
@@ -0,0 +1,36 @@
+import Libraries.baseImplementation
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "momolucas.core"
+    compileSdk = Config.compileSdk
+
+    defaultConfig {
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    baseImplementation()
+}
